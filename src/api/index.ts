8c8a8e259e6447767453eb154e518fa215124123
Jason <jasonlucas443@gmail.com>
2024-06-30 21:23:44 -0300

    fix: ajustando apontamentos das variaveis de ambiente

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,7 +3,12 @@ import { useLocalStorage } from '../hooks/useLocalStorage';
 
 const { get } = useLocalStorage();
 
-export const api = axios.create({ baseURL: import.meta.env.VITE_API_URL });
+export const api = axios.create({ 
+    baseURL: import.meta.env.ENVIRONMENT === 'production' 
+    ? import.meta.env.REACT_APP_API_URL 
+    : import.meta.env.VITE_API_URL 
+});
+
 api.interceptors.request.use((request) => {
     request.headers['x-session-id'] = get('access_token');
     return request;
